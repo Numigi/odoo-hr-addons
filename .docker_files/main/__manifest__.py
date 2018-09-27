9561a52828155703faefc1e9b3a06f66828981c1
David Dufresne <david.dufresne@numigi.net>
2018-09-27 11:57:00 -0400

    Add module hr_contract_wage_type

--- .docker_files/main/__manifest__.py
+++ .docker_files/main/__manifest__.py
@@ -11,6 +11,7 @@
     'category': 'Other',
     'summary': 'Install all addons required for testing.',
     'depends': [
+        'hr_contract_wage_type',  # TA#3893
         'hr_event',  # TA#3122
         'hr_working_space',  # TA#3896
         'hr_holidays_half_day',  # TA#1739
---
+++ hr_contract_wage_type/__manifest__.py
@@ -0,0 +1,19 @@
+# © 2018 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'HR Contract Wage Type',
+    'version': '1.0.0',
+    'author': 'Numigi',
+    'maintainer': 'Numigi',
+    'license': 'LGPL-3',
+    'category': 'Human Resources',
+    'summary': 'Distinguish between hourly / monthly / yearly wages',
+    'depends': [
+        'hr_contract',
+    ],
+    'data': [
+        'views.xml',
+    ],
+    'installable': True,
+}
