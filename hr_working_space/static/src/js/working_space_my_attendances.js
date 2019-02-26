09c5fae352f82268fbcf3960ab3882af512f81ae
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2019-02-25 20:54:11 -0500

    hr_working_space simplify widget extension
    Intead of rewriting the widget code, extend the base widget from hr_attendance.
    In future versions of Odoo, html/css/js elements will need less adjustments.

--- hr_working_space/static/src/js/working_space_my_attendances.js
+++ hr_working_space/static/src/js/working_space_my_attendances.js
@@ -1,69 +1,39 @@
-odoo.define("hr_working_space.working_space_my_attendances", function (require) {
+odoo.define("hr_working_space.working_space_my_attendances", (require) => {
     "use strict";
 
     var core = require("web.core");
-    var AbstractAction = require("web.AbstractAction");
+    var AttendanceWidget = require('hr_attendance.my_attendances');
 
-    var QWeb = core.qweb;
-    var _t = core._t;
-
-
-    var MyAttendancesWorkingSpace = AbstractAction.extend({
-        events: {
-            "click .o_hr_working_space_sign_in_out_icon": function (event) {
-                this.$(".o_hr_working_space_sign_in_out_icon").attr("disabled", "disabled");
-                this.update_attendance(event);
+    AttendanceWidget.include({
+        events: _.extend({}, AttendanceWidget.prototype.events, {
+            "click .o_hr_working_space_sign_in": function (event) {
+                this.sign_in_with_working_space(event);
             },
-        },
-
-        start: function () {
-            var self = this;
-
+        }),
+        start() {
             var def = this._rpc({
-                model: "hr.employee",
-                method: "search_read",
-                args: [[["user_id", "=", this.getSession().uid]], ["attendance_state", "name"]],
-            }).then(function (res) {
-                if (_.isEmpty(res)) {
-                    self.$(".o_hr_attendance_employee").append(_t("Error : Could not find employee linked to user"));
-                    return;
-                }
-                self.employee = res[0];
-            });
-
-            var def2 = self._rpc({
                 model: "hr.working.space",
                 method: "search_read",
                 fields: ["name", "icon"]
-            }).then(function (workingSpaces) {
-                self.workingSpaces = workingSpaces;
+            }).then((workingSpaces) => {
+                this.workingSpaces = workingSpaces;
             });
-
-            return $.when(this._super.apply(this, arguments), def, def2).done(function(){
-                self.$el.html(QWeb.render("HrWorkingSpaceMyMainMenu", {widget: self}));
-            });
+            return $.when(def, this._super.apply(this, arguments));
         },
-
-        update_attendance: function (event) {
-            var self = this;
+        sign_in_with_working_space(event) {
             var workingSpaceID = event.target.id;
             this._rpc({
                 model: "hr.employee",
                 method: "attendance_manual_working_space",
-                args: [[self.employee.id], "hr_attendance.hr_attendance_action_my_attendances", workingSpaceID]
+                args: [[this.employee.id], "hr_attendance.hr_attendance_action_my_attendances", workingSpaceID]
             })
-                .then(function (result) {
-                    if (result.action) {
-                        self.do_action(result.action);
-                    } else if (result.warning) {
-                        self.do_warn(result.warning);
-                    }
-                });
+            .then((result) => {
+                if (result.action) {
+                    this.do_action(result.action);
+                } else if (result.warning) {
+                    this.do_warn(result.warning);
+                }
+            });
         },
     });
-
-    core.action_registry.add("hr_working_space_my_attendances", MyAttendancesWorkingSpace);
-
-    return MyAttendancesWorkingSpace;
-
 });
