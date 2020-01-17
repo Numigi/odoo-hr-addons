fcc9655fa8ea76f3ef74e391c98a4eb1e2d41c0f
Jordi Riera <jordi.riera@numigi.com>
2020-01-17 14:27:27 -0500

    simplification of the code using api.constrains

--- hr_expense_same_month/models.py
+++ hr_expense_same_month/models.py
@@ -10,57 +10,16 @@ _logger = logging.getLogger(__name__)
 class HrExpenseSheetConstrain(models.Model):
     _inherit = 'hr.expense.sheet'
 
-    @api.model
-    def create(self, vals_list):
-        if not self._are_expenses_same_months(vals_list):
-            self._raise_expenses_error()
+    @api.constrains('expense_line_ids')
+    def _are_expenses_same_months(self):
+        for record in self:
+            months = set()
 
-        return super().create(vals_list)
+            for line in record.expense_line_ids:
+                months.add(line.date.month)
 
-    @api.multi
-    def write(self, vals):
-        if not self._are_expenses_same_months(vals):
-            self._raise_expenses_error()
-
-        return super().write(vals)
-
-    def _are_expenses_same_months(self, vals):
-        expense_line_field = "expense_line_ids"
-
-        if expense_line_field in vals:
-            expenses = []
-
-            for line in vals[expense_line_field]:
-                expense_details = line[-1]
-
-                if isinstance(expense_details, list):
-                    expenses.extend(expense_details)
-                else:
-                    expenses.append(expense_details)
-
-            months = self._expense_months(expenses)
-            return not len(months) > 1
-        return True
-
-    def _expense_months(self, expenses):
-        months = set()
-        for entry in expenses:
-
-            if isinstance(entry, dict):
-                # date is required in hr.expense
-                date = entry["date"]
-                month = date.split("-")[1]
-                months.add(month)
-
-            else:
-                expense = self.env["hr.expense"].browse(entry)
-                if expense:
-                    months.add(expense.date.month)
-
-        return months
-
-    @staticmethod
-    def _raise_expenses_error():
-        raise ValidationError(_(
-            "All expenses must be from the same month. Please review the list of expenses."
-        ))
+            if len(months) > 1:
+                raise ValidationError(_(
+                    "All expenses must be from the same month."
+                    " Please review the list of expenses."
+                ))
