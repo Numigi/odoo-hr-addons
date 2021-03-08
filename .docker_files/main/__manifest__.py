646b39455bf1820a89d0dc56b7c8011f7b15fc7c
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2021-03-08 15:31:58 -0500

    TA#31713 add module hr_attendance_menu_unrestricted (#20)

--- .docker_files/main/__manifest__.py
+++ .docker_files/main/__manifest__.py
@@ -2,24 +2,25 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
-    'name': 'Main Module',
-    'version': '1.0.0',
-    'author': 'Numigi',
-    'maintainer': 'Numigi',
-    'website': 'https://www.numigi.com',
-    'license': 'LGPL-3',
-    'category': 'Other',
-    'summary': 'Install all addons required for testing.',
-    'depends': [
-        'hr_contract_single_open',
-        'hr_contract_wage_type',  # TA#3893
-        'hr_employee_private_wizard',
-        'hr_employee_type',  # TA#15755
-        'hr_employee_type_private_wizard',
-        'hr_event',  # TA#3122
-        'hr_expense_same_month',  # TA#18858
-        'hr_payslip_chatter',  # TA#24250
-        'hr_working_space',  # TA#3896
+    "name": "Main Module",
+    "version": "1.0.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "website": "https://www.numigi.com",
+    "license": "LGPL-3",
+    "category": "Other",
+    "summary": "Install all addons required for testing.",
+    "depends": [
+        "hr_attendance_menu_unrestricted",
+        "hr_contract_single_open",
+        "hr_contract_wage_type",  # TA#3893
+        "hr_employee_private_wizard",
+        "hr_employee_type",  # TA#15755
+        "hr_employee_type_private_wizard",
+        "hr_event",  # TA#3122
+        "hr_expense_same_month",  # TA#18858
+        "hr_payslip_chatter",  # TA#24250
+        "hr_working_space",  # TA#3896
     ],
-    'installable': True,
+    "installable": True,
 }
---
+++ hr_attendance_menu_unrestricted/__manifest__.py
@@ -0,0 +1,20 @@
+# © 2020 - Today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "HR Attendance Menu Unrestricted",
+    "version": "1.0.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "license": "LGPL-3",
+    "category": "Human Resources",
+    "summary": "Add attendance menus to employees",
+    "depends": [
+        "hr_attendance",
+    ],
+    "data": [
+        "views/menu.xml",
+        "security/ir_rule.xml",
+    ],
+    "installable": True,
+}
