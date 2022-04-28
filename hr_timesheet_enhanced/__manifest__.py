7ce52f9bc159c3c97406d70fa5f9d4fa75f89042
cmoa <cmoa@cmoaaa>
2022-04-28 19:33:10 +0300

    TA#43968
    add new module hr_timesheet_enhanced

---
+++ hr_timesheet_enhanced/__manifest__.py
@@ -0,0 +1,19 @@
+# © 2022 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Hr Timesheet Enhanced",
+    "version": "1.0.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "website": "https://bit.ly/numigi-com",
+    "license": "LGPL-3",
+    "category": "Human Resources",
+    "summary": """Add automations with value of active field and deletion of associated project""",
+    "depends": [
+        "project_enhanced",
+        "hr_timesheet",
+    ],
+    "data": [],
+    "installable": True,
+}
