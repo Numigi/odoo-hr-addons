487cfc5f1e05aafda3acca07cdb0d3d948196d90
majouda <majdaelmariouli@gmail.com>
2023-01-04 00:04:20 -0500

    TA#46133 add module hr_timesheet_time_control_stop_at_checkout

--- hr_timesheet_time_control_stop_at_checkout/models/hr_employee.py
+++ hr_timesheet_time_control_stop_at_checkout/models/hr_employee.py
@@ -1,8 +1,7 @@
 # © 2022 - Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import models, fields, api, exceptions, _, SUPERUSER_ID
-from odoo.exceptions import UserError
+from odoo import models, api
 
 
 class HrEmployee(models.Model):
