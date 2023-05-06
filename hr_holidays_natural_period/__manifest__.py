60f34688ca58d836444382b0f5d4c6b11c2d1c99
Lanto Razafindrabe <lanto.razafindrabe@numigi.com>
2023-05-06 08:41:29 +0300

    TA#52493 [CHG][ADD] hr_holidays_natural_period : readme  file changed and screenshot added

--- hr_holidays_natural_period/__manifest__.py
+++ hr_holidays_natural_period/__manifest__.py
@@ -7,7 +7,7 @@
     "version": "12.0.1.0.0",
     "category": "Human Resources",
     "website": "https://github.com/OCA/hr-holidays",
-    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "author": "Numigi, Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "installable": True,
     "depends": ["hr_holidays"],
