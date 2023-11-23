d24b2670271d59f5c42bc052cef684e4f838f640
Lanto R <124650562+lanto-razafindrabe@users.noreply.github.com>
2023-11-23 19:44:31 +0300

    TA#51920 [IMP] hr_timesheet_enhanced : readme and tracking (#56)
    * TA#51920 [IMP] hr_timesheet_enhanced : Adjust readme and add tracking

--- hr_timesheet_enhanced/__manifest__.py
+++ hr_timesheet_enhanced/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Hr Timesheet Enhanced",
-    "version": "14.0.1.0.1",
+    "version": "14.0.1.0.2",
     "author": "Numigi",
     "maintainer": "Numigi",
     "website": "https://bit.ly/numigi-com",
