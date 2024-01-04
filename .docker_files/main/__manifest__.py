af6c923a70bad14395a490d5c52392228e1aa38b
Abdellatif Benzbiria <a.benzbiria@gmail.com>
2024-01-04 21:45:30 +0100

    TA#58633[ADD] hr_payroll_canadian_declaration : new module

--- .docker_files/main/__manifest__.py
+++ .docker_files/main/__manifest__.py
@@ -25,6 +25,7 @@
         "hr_expense_same_month",  # TA#18858
         "hr_holidays_natural_period",  # TA#52493
         "hr_payroll_account_partner",
+        "hr_payroll_canadian_declaration",  # TA#58633
         "hr_payroll_report",
         "hr_payslip_chatter",  # TA#24250
         "hr_working_space",  # TA#3896
