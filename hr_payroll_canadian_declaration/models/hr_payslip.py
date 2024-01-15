c23ae011f5551e0b585792a30a37c81290046fb8
Majda EL MARIOULI <majdaelmariouli@gmail.com>
2024-01-15 11:59:33 -0500

    Fixe Flake8 error

--- hr_payroll_canadian_declaration/models/hr_payslip.py
+++ hr_payroll_canadian_declaration/models/hr_payslip.py
@@ -1,7 +1,7 @@
 # © 2023 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import models, fields, api
+from odoo import models, fields
 
 
 class HrPayslipLine(models.Model):
@@ -10,9 +10,3 @@ class HrPayslipLine(models.Model):
 
     t4_box = fields.Text(related='salary_rule_id.t4_box', index=True, store=True, string="T4 Box")
     r1_box = fields.Text(related='salary_rule_id.r1_box', index=True, store=True, string="R1 Box")
-
-
-
-
-
-
