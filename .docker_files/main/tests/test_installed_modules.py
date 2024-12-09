e31487307d6ee48724505ea320432fb68e2fe3e3
Rivo Lalaina <rivo2302@gmail.com>
2024-12-09 21:43:12 +0300

    [IMP] [16.0] pre-commit : black hook (#78)

--- .docker_files/main/tests/test_installed_modules.py
+++ .docker_files/main/tests/test_installed_modules.py
@@ -13,12 +13,12 @@ class TestModules(TransactionCase):
 
     def setUp(self):
         super(TestModules, self).setUp()
-        self.modules = self.env['ir.module.module']
+        self.modules = self.env["ir.module.module"]
 
     def test_hr_event(self):
         """HR Event is installed."""
-        self.assertTrue(self.modules.search([('name', '=', 'hr_event')]))
+        self.assertTrue(self.modules.search([("name", "=", "hr_event")]))
 
     def test_hr_working_space(self):
         """Working Space is installed."""
-        self.assertTrue(self.modules.search([('name', '=', 'hr_working_space')]))
+        self.assertTrue(self.modules.search([("name", "=", "hr_working_space")]))
